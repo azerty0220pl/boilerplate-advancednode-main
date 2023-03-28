dcb7ea0272dcc3da1e0530c9bc200d622d9d30f3
Szymon Kokot <azerty0220pl@gmail.com>
2023-03-28 12:41:19 +0200

    exercise6

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const session = require('express-session');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const { ObjectID } = require('mongodb');
+const LocalStrategy = require('passport-local');
 
 const app = express();
 
@@ -31,7 +32,17 @@ app.use(express.urlencoded({ extended: true }));
 
 myDB(async client => {
   const myDataBase = await client.db('database').collection('users');
-
+  
+  passport.use(new LocalStrategy((username, password, done) => {
+    myDataBase.findOne({ username: username }, (err, user) => {
+      console.log(`User ${username} attempted to log in.`);
+      if (err) return done(err);
+      if (!user) return done(null, false);
+      if (password !== user.password) return done(null, false);
+      return done(null, user);
+    });
+  }));
+  
   app.route('/').get((req, res) => {
     res.render('index', {
       title: 'Connected to Database',
